348e5296dafc9ff86c85ef77bb727cccd44d7257
MRX <sureshmadawa763@gmail.com>
2023-12-19 21:37:45 +0530

    About section / services section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,102 @@
+"use client";
 import React from "react";
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
+import Link from "next/link";
+
+const statusData = [
+  {
+    firstLineText: "Year of",
+    secondLineText: "Experience",
+    start: 0,
+    end: 3,
+    duration: 3,
+  },
+  {
+    firstLineText: "Projects",
+    secondLineText: "Completed",
+    start: 0,
+    end: 100,
+    duration: 3,
+  },
+  {
+    firstLineText: "Satisfied",
+    secondLineText: "Clients",
+    start: 0,
+    end: 50,
+    duration: 3,
+  },
+];
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div id="about" className="section">
-      About
-    </div>
+    <section id="about" ref={ref} className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col h-screen gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0">
+          {/* img */}
+          <motion.div
+            variants={fadeIn("right", 0.3)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1 bg-no-repeat bg-contain bg-about h-[640px] mix-blend-lighten bg-top"
+          ></motion.div>
+          {/* text */}
+          <motion.div
+            variants={fadeIn("left", 0.5)}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.3 }}
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">About me.</h2>
+            <h3 className="mb-4 h3">
+              I'm a freelancer Front-end Developer with over 5 years of
+              experience.
+            </h3>
+            <p className="mb-6">
+              Tempor excepteur velit elit consequat nulla mollit fugiat occaecat
+              esse officia cillum enim. Exercitation sint mollit id adipisicing
+              irure. Proident commodo enim cupidatat sit pariatur nisi ex
+              adipisicing dolor excepteur nisi culpa laboris. Do consequat
+              proident velit magna sint.
+            </p>
+            {/* states */}
+            <div className="flex mb-12 gap-x-6 lg:gap-x-10">
+              {statusData.map((item, index) => (
+                <div key={`status${index}`}>
+                  <div className="text-[40px] font-tertiary text-gradient mb-2">
+                    {inView ? (
+                      <CountUp
+                        start={item.start}
+                        end={item.end}
+                        duration={item.duration}
+                      />
+                    ) : null}
+                  </div>
+                  <div className="text-sm font-primary tracking-[2px]">
+                    {item.firstLineText}
+                    <br />
+                    {item.secondLineText}
+                  </div>
+                </div>
+              ))}
+            </div>
+            <div className="flex items-center gap-x-8">
+              <button className="btn btn-lg">Contact me</button>
+              <Link href={""} className="text-gradient btn-link">
+                My Portfolio
+              </Link>
+            </div>
+          </motion.div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,10 +1,90 @@
+"use client";
 import React from "react";
+import { BsArrowUpRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "@/variants";
+import Link from "next/link";
+
+// services data
+const services = [
+  {
+    name: "UI / UX",
+    description:
+      "Veniam incididunt labore excepteur ipsum cupidatat deserunt. Magna non veniam non enim et",
+    link: "Learn more",
+  },
+  {
+    name: "Development",
+    description:
+      "Veniam incididunt labore excepteur ipsum cupidatat deserunt. Magna non veniam non enim et",
+    link: "Learn more",
+  },
+  {
+    name: "Digital Marketing",
+    description:
+      "Veniam incididunt labore excepteur ipsum cupidatat deserunt. Magna non veniam non enim et",
+    link: "Learn more",
+  },
+  {
+    name: "Product Branding",
+    description:
+      "Veniam incididunt labore excepteur ipsum cupidatat deserunt. Magna non veniam non enim et",
+    link: "Learn more",
+  },
+];
 
 const Services = () => {
   return (
-    <div id="services" className="section">
-      Services
-    </div>
+    <section id="services" className="section">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+          {/* text & images */}
+          <motion.div className="flex-1 mb-12 bg-no-repeat lg:bg-services lg:bg-bottom mix-blend-lighten lg:mb-0">
+            <h2 className="mb-6 text-accent h2">What I Do</h2>
+            <h3 className="h3 max-w-[455px] mb-16">
+              I'm a freelancer Front-end Developer with over 5 years of
+              experience.
+            </h3>
+            <button className="btn btn-sm">See my work</button>
+          </motion.div>
+          {/* services */}
+          <div className="">
+            {/* service list */}
+            <div className="flex-1">
+              {services.map((service, index) => {
+                const { name, description, link } = service;
+                return (
+                  <div
+                    key={`service${index}`}
+                    className="border-b border-white/20 h-[146px] mb-[38px] flex"
+                  >
+                    <div className="max-w-[476px]">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-6">
+                        {name}
+                      </h4>
+                      <p className="leading-tight font-secondary">
+                        {description}
+                      </p>
+                    </div>
+                    <div className="flex flex-col items-end flex-1">
+                      <Link
+                        href={""}
+                        className="btn w-9 h-9 mb-[42px] flex justify-center items-center"
+                      >
+                        <BsArrowUpRight />
+                      </Link>
+                      <Link href={""} className="text-sm text-gradient">
+                        {link}
+                      </Link>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
