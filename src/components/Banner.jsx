db072125ebdf4176c918168681adc94cde6d2bf8
MRX <sureshmadawa763@gmail.com>
2023-12-26 11:02:37 +0530

    image size down

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import bannerImg from "@/assets/avatar.svg";
+import bannerImg from "@/assets/avatar.png";
 import { FaGithub, FaYoutube, FaDribbble } from "react-icons/fa";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
